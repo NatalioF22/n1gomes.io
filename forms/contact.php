1b851655b46b636149938422e9bed641f9002aa0
NatalioF22 <nataliogomes707@gmail.com>
2023-04-14 19:51:11 -0400

    initial commit

---
+++ forms/contact.php
@@ -0,0 +1,40 @@
+
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "nataliogomes707@gmail.com";
+$mail->Password = "szdcjhqrycvnleir";
+
+$mail->setFrom($email, $name);
+$addr = $_POST['email'];
+$mail->addAddress($addr, "Dave");
+
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
